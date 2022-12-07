fa88560d8c35d06dde11d16172b34d2497150a34
Devildaus <devildaus666@gmail.com>
2022-12-07 08:41:21 +0700

    first

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -29,60 +32,10 @@ Route::get('/about', function(){
 });
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Post 1",
-            "slug" => "post-1",
-            "author" => "Firdaus",
-            "isi" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, reprehenderit rem. Expedita ipsum numquam tempora, recusandae accusantium ipsam consectetur et exercitationem corrupti officia cumque obcaecati, ad tenetur deleniti aspernatur enim."
-        ],
-        [
-            "title" => "Post 2",
-            "slug" => "post-2",
-            "author" => "Atif",
-            "isi" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, reprehenderit rem. Expedita ipsum numquam tempora, recusandae accusantium ipsam consectetur et exercitationem corrupti officia cumque obcaecati, ad tenetur deleniti aspernatur enim."
-        ],
-    ];
-    return view('posts', [
-        "title" => "Blog",
-        "posts" => $blog_posts
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
 
 
 
 //Halaman Single post
 
-Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "Post 1",
-            "slug" => "post-1",
-            "author" => "Firdaus",
-            "isi" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, reprehenderit rem. Expedita ipsum numquam tempora, recusandae accusantium ipsam consectetur et exercitationem corrupti officia cumque obcaecati, ad tenetur deleniti aspernatur enim."
-        ],
-        [
-            "title" => "Post 2",
-            "slug" => "post-2",
-            "author" => "Atif",
-            "isi" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, reprehenderit rem. Expedita ipsum numquam tempora, recusandae accusantium ipsam consectetur et exercitationem corrupti officia cumque obcaecati, ad tenetur deleniti aspernatur enim."
-        ],
-    ];
-
-    $new_post = [];
-
-    foreach ($blog_posts as $post) {
-        if ($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-
-
-    return view('post', [
-        "title" => "single post",
-        "post" => $new_post
-    ]);
-    
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
