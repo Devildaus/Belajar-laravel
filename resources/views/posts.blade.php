7113b2fd6feb41d87a9f6c84f13b8ad9aa854273
Devildaus <devildaus666@gmail.com>
2022-12-07 08:50:29 +0700

    wow

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,11 +4,14 @@
 
 @foreach ($posts as $post)
     <article class mb-5>
-        <a href="/posts/{{ $post{'slug'} }}">
-            <h2>{{ $post["title"] }}</h2>
+        <a href="/posts/{{ $post->slug }}">
+            <h2>{{ $post->title }}</h2>
         </a> 
-        <h5>By : {{ $post["author"] }}</h5>
-        <p>{{ $post["isi"] }}</p>
+
+        {{ $post->excerpt }}
+        <a href="/posts/{{ $post->slug }}">
+            ..... <h5>Selengkapnya</h5>
+        </a></br></br> 
     </article>   
 @endforeach
 
