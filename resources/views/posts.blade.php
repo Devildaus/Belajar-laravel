c8b0d2a7f7c78341b4b075d0abacc293213c9476
Devildaus <devildaus666@gmail.com>
2022-12-18 21:00:38 +0700

    video 10

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,7 +8,7 @@
         <a href="/posts/{{ $post->slug }}">
             <h2>{{ $post->title }}</h2>
         </a> 
-        <p>By.<a href="#">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug ?? 'unknown' }}" class="text-decoration-none">  {{ $post->category->name  ?? 'unknown' }}</a></p> 
+        <p>By.<a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug ?? 'unknown' }}" class="text-decoration-none">  {{ $post->category->name  ?? 'unknown' }}</a></p> 
 
         {{ $post->excerpt }}
         <a href="/posts/{{ $post->slug }}">
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-use App\Models\category;
+use App\Models\Category;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -54,6 +55,13 @@ Route::get('/categories/{category:slug}', function (Category $category){
         'title'=>$category->name,
         'post'=>$category->posts,
         'category'=>$category->name
+    ]);
+});
+
+Route::get('/authors/{author:username}', function(User $author) {
+    return view('posts', [
+        'title' => 'All Posts by '.$author->name.' :',
+        'posts' => $author->posts,
     ]);
 });
 
